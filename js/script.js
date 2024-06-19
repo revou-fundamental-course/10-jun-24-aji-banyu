4efc5c72ef8e305df228489f5774dd3329cf8b6f
aji-banyu <ajibanyurizqi@gmail.com>
2024-06-19 10:50:16 +0700

    bug

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// function reset button#276873
-function resetValues() {
-  document.getElementById("celcius").value = "";
-  document.getElementById("fahrenheit").value = "";
-  document.getElementById("calculate").value = "";
-}
